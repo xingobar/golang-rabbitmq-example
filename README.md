138d84fa66397299146aaa3c1a9ac7cf0ca9f1d1
xingobar <gary840227@gmail.com>
2020-09-13 12:05:31 +0800

    rabbitmq send example message

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # golang-rabbitmq-example
 golang rabbitmq example
 
+1. 宣告 exchange
+2. 宣告 queue (Routing key)
+3. 綁定 Exchange 和 Queue
+4. 發布信息到 exchange 且指定 routing key, routing key 會將訊息送至指定的 queue
+
+run:
+1. go build main.go
+2. ./main  -> 會發送消息至 exchange_1
+
 package:
 1.  go get github.com/streadway/amqp
--- main.go
+++ main.go
@@ -1,9 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"github.com/streadway/amqp"
+)
+
+const (
+	queueName = "hello"
+	exchange = "exchange1"
+	mqUrl = "amqp://guest:guest@localhost:5672/"
+)
+
+var channel *amqp.Channel
+var conn *amqp.Connection
 
 func main() {
+	push()
+	receive()
+	close()
+}
 
+func push() {
+	if channel == nil {
+		mqConnect()
+	}
+
+	mgsConnect := "hello world"
+	err := channel.ExchangeDeclare(exchange, "direct", false, false, false, false, nil)
+	failOnError(err, " exchange declare")
+
+	_, err = channel.QueueDeclare(queueName, false, false, false, false, nil)
+	failOnError(err, " queue declare")
+
+	err = channel.QueueBind(queueName, "info", exchange, false, nil)
+	failOnError(err, " queue bind")
+
+
+	err = channel.Publish(exchange, "info", false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body: []byte(mgsConnect),
+	})
+	failOnError(err, " Publish")
+	fmt.Println("push ok")
 }
 
 func failOnError(err error, msg string) {
@@ -13,3 +52,39 @@ func failOnError(err error, msg string) {
 	}
 }
 
+// rabbitmq 連線
+func mqConnect() {
+	// 連接 rabbitmq
+	var err error
+	conn, err = amqp.Dial(mqUrl)
+	failOnError(err, "mqconnect")
+
+	// 建立 channel
+	channel, err = conn.Channel()
+	failOnError(err, "channel")
+}
+
+// rabbitmq close
+func close() {
+	fmt.Println("rabbitmq close connection .....")
+	channel.Close()
+	conn.Close()
+}
+
+func receive() {
+	if channel == nil {
+		mqConnect()
+	}
+	msg, ok, err := channel.Get(queueName, false)
+
+	failOnError(err, "")
+	if !ok {
+		fmt.Println("do not get msg")
+		return
+	}
+
+	err = channel.Ack(msg.DeliveryTag, true)
+	failOnError(err, "")
+
+	fmt.Println(bytes.NewBuffer(msg.Body).String())
+}
